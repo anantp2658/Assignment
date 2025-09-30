deafb36437c2615a3909d4ca5d523372e69370cc
anantp2658 <141740834+anantp2658@users.noreply.github.com>
2025-09-30 18:43:53 +0530

    Add files via upload

--- Assignment_19.java
+++ Assignment_19.java
@@ -2,5 +2,15 @@ package assignments;
 //WAP for Local variables
 public class Assignment_19 
 {
-	
+	static void add()
+	{
+		int a=10; //local variable
+		int b=11; //local variable
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) 
+	{
+		add();
+	}
 }
